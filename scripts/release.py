e2b93352a67484cb0907ba0ef0f781fc4a33361d
Alex Zheng <cnzhenghan@gmail.com>
2026-05-02 16:06:25 +0800

    chore: add AlexZHENGH to AUTHOR_MAP for contributor attribution check

--- scripts/release.py
+++ scripts/release.py
@@ -644,6 +644,8 @@ AUTHOR_MAP = {
     "web3blind@gmail.com": "web3blind",
     "ztzheng@163.com": "chengoak",  # PR #17467
     "24110240104@m.fudan.edu.cn": "YuShu",  # co-author only
+    # alex-custom fork (May 2026)
+    "cnzhenghan@gmail.com": "AlexZHENGH",
 }
 
 
